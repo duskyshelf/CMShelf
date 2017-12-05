9d651024bf4574d8b56ee74eced08e43c963ac26
David Upsdale <dupsdale@gmail.com>
2017-12-05 16:09:52 +0000

    updates content site to have multiple content blocks

---
+++ demo/content-site/src/ContentBlock.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import fetch from 'isomorphic-fetch'
+
+class ContentBlock extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { data: "no data" };
+    this.id = this.props.contentId;
+  }
+
+  componentDidMount() {
+    fetch(`http://localhost:3100/id/${this.id}`)
+      .then(response => response.json())
+      .then(json => this.setState( { data: json[0].content }))
+  }
+
+  render() {
+    return (
+        <p className={`App-intro content-${this.id}`}>
+          { this.state.data }
+        </p>
+    );
+  }
+}
+
+export default ContentBlock;
