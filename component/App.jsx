b1263499f2e36803bfe67abfff839a61e5702890
DuskyShelf <dupsdale@gmail.com>
2018-01-02 01:08:47 +0000

    adds hide button with styling

--- component/App.jsx
+++ component/App.jsx
@@ -1,29 +1,57 @@
 import React, { Component } from 'react'
 import * as ReactDOM from 'react-dom'
-import RichTextEditor from 'react-rte';
+import RichTextEditor from 'react-rte'
 import RichEditor from './RichEditor'
 import SubmitButton from './SubmitButton'
+
+const hideButtonStyle = {
+  position: 'absolute',
+  top: 0,
+  right: 0
+}
+
+const containerStyle = {
+  position: 'relative'
+}
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      visible: true,
       content: RichTextEditor.createValueFromString(props.content, 'html'),
       id: props.id
     };
 
     this.onChange = this.onChange.bind(this);
+    this.toggleView = this.toggleView.bind(this);
   }
 
   onChange(content) {
     this.setState({ content });
   }
 
+  toggleView() {
+    this.setState({ visible: !this.state.visible })
+  }
+
+
+
   render() {
+    const { content, id, visible } = this.state
+
     return (
-      <div>
-        <RichEditor value={this.state.content} onChange={this.onChange} />
-        <SubmitButton value={this.state.content} contentId={this.state.id} />
+      <div style={containerStyle}>
+        <button className='cmshelf-hide-button' onClick={this.toggleView} style={hideButtonStyle}>hide/show</button>
+        { visible &&
+          <div>
+            <RichEditor value={content} onChange={this.onChange} />
+            <SubmitButton value={content} contentId={id} />
+          </div>
+        }
+        { !visible &&
+          <div dangerouslySetInnerHTML={{ __html: content.toString('html') }} />
+        }
       </div>
     )
   }
